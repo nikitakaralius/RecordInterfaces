7907e166dd74bb7923ac78507676780ee9a2ab48
Каралюс Никита <nkaralius@data-platform.ru>
2024-04-04 23:20:39 +0300

    Fixed many properties default case

---
+++ src/RecordInterfaces.Sample/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using RecordInterfaces.Sample;
+
+IUser user = new DefaultUser
+{
+    FirstName = "Old first name",
+    LastName = "Old last name",
+    MiddleName = "Old last name"
+};
+
+user = user.With(firstName: "New first name");
+
+Console.WriteLine(user);
+
--- src/RecordInterfaces.Sample/Types.cs
+++ src/RecordInterfaces.Sample/Types.cs
@@ -1,3 +1,4 @@
+using Microsoft.CodeAnalysis;
 using RecordInterfaces.Abstractions;
 
 namespace RecordInterfaces.Sample;
@@ -5,19 +6,17 @@ namespace RecordInterfaces.Sample;
 [RecordInterface]
 public partial interface IUser
 {
-    Guid Id { get; }
     string FirstName { get; }
     string LastName { get; }
     string? MiddleName { get; }
-    int Age { get; }
 }
 
 [ImplementsRecordInterface]
 public sealed partial record DefaultUser : IUser
 {
-    public Guid Id { get; init; }
     public string FirstName { get; init; } = string.Empty;
+
     public string LastName { get; init; } = string.Empty;
+
     public string? MiddleName { get; init; }
-    public int Age { get; init; }
 }
